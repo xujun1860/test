319a0d14b98dc39682a33b0cf696032283f6f02f
xujun <xujun1860@163.com>
2013-09-06 16:56:13 +0800

    asd

--- test/ViewController.m
+++ test/ViewController.m
@@ -17,8 +17,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    NSLog(@"dasdas");
-
+    NSLog(@"111");
+    NSLog(@"222");
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
