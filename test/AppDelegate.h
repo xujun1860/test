9b7d63c36996897003dd953c53ab8b08f65b76b8
xujun <xujun1860@163.com>
2013-09-06 16:29:07 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 徐 军 on 13-9-6.
+//  Copyright (c) 2013年 chenjin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
